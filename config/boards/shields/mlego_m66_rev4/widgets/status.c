8e9d82947053b32b31c9e58c5e43f462418ec9a6
alin elena <alin@elena.re>
2024-03-24 09:28:50 +0000

    ls013b7dh05 nicenano r90

--- config/boards/shields/mlego_m66_rev4/widgets/status.c
+++ config/boards/shields/mlego_m66_rev4/widgets/status.c
@@ -335,10 +335,11 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
     lv_obj_align(top, LV_ALIGN_TOP_RIGHT, 0, 0);
     lv_canvas_set_buffer(top, widget->cbuf, CANVAS_SIZE, CANVAS_SIZE, LV_IMG_CF_TRUE_COLOR);
     lv_obj_t *middle = lv_canvas_create(widget->obj);
-    lv_obj_align(middle, LV_ALIGN_TOP_LEFT, 24, 0);
+
+    lv_obj_align(middle, LV_ALIGN_TOP_LEFT, 0,24);
     lv_canvas_set_buffer(middle, widget->cbuf2, CANVAS_SIZE, CANVAS_SIZE, LV_IMG_CF_TRUE_COLOR);
     lv_obj_t *bottom = lv_canvas_create(widget->obj);
-    lv_obj_align(bottom, LV_ALIGN_TOP_LEFT, -24, 0);
+    lv_obj_align(bottom, LV_ALIGN_TOP_LEFT,0, -24);
     lv_canvas_set_buffer(bottom, widget->cbuf3, CANVAS_SIZE, CANVAS_SIZE, LV_IMG_CF_TRUE_COLOR);
     sys_slist_append(&widgets, &widget->node);
     widget_battery_status_init();
