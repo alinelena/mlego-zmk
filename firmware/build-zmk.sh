2a4d4a654a8cca100ce22503ebab3f02f18f6928
alin elena <alin@elena.re>
2024-03-30 19:24:00 +0000

    sleep... and wake up source

--- firmware/build-zmk.sh
+++ firmware/build-zmk.sh
@@ -24,20 +24,20 @@ shield="mlego5x13_rev8"
 #shield="mlego_m66_rev4_ls013b7dh03"
 #shield="mlego_m66_rev4_ls013b7dh05"
 #shield="mlego_m66_rev4_eink154"
-for s in "" "_ls011b7dh03" "_ls013b7dh03" "_ls013b7dh05" "_eink154" "_eink213"; do
+#for s in "" "_ls011b7dh03" "_ls013b7dh03" "_ls013b7dh05" "_eink154" "_eink213"; do
 # for s in "_eink154"; do
 #for s in "_eink213"; do
-#for s in "_ls013b7dh05" ; do
+for s in "ls013b7dh05" ; do
 #for s in "_ls013b7dh03" ; do
 #for s in "_ls011b7dh03" ; do
-shield="mlego_m66_rev4$s"
+shield="mlego_m66_rev4 $s"
 board="nice_nano_v2"
 build_folder="mlego_m66_rev4$shield"
 
 
 rm -rf $build_folder
 
-west build -d $build_folder -p always -b $board -- -DSHIELD=$shield -DZMK_CONFIG=$zmk_config
+west build -d $build_folder -p always -b $board -- -DSHIELD="$shield" -DZMK_CONFIG=$zmk_config
 [[ -f $build_folder/zephyr/zmk.uf2 ]] && cp $build_folder/zephyr/zmk.uf2 $l/$build_folder.uf2
 done
 while [ ! -d /run/media/drFaustroll/NICENANO/ ]; do
