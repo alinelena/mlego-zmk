904aa093b4e8a980d16c0f828c834147eec9f6b7
alin elena <alin@elena.re>
2024-04-07 10:16:39 +0100

    updated

--- firmware/build-zmk.sh
+++ firmware/build-zmk.sh
@@ -24,22 +24,19 @@ shield="mlego5x13_rev7"
 #for s in "" "_ls011b7dh03" "_ls013b7dh03" "_ls013b7dh05" "_eink154" "_eink213"; do
 # for s in "_eink154"; do
 #for s in "_eink213"; do
-#for s in "ls013b7dh05" ; do
+for s in "ls013b7dh05" ; do
 #for s in "ls013b7dh03s" ; do
 #for s in "ls011b7dh03" ; do
 shield="mlego_m66_rev4 $s"
 board="nice_nano_v2"
 build_folder="mlego_m66_rev4${shield/ /_}"
-build_folder="xiao_rev9"
-board="seeeduino_xiao_rp2040"
-shield="mlego5x13_rev9"
 
 
 rm -rf "$build_folder"
 
 west build -d "$build_folder" -p always -b $board -- -DSHIELD="$shield" -DZMK_CONFIG=$zmk_config
 [[ -f "$build_folder/zephyr/zmk.uf2" ]] && cp "$build_folder/zephyr/zmk.uf2" "$l/$build_folder.uf2"
-# done
+done
 while [ ! -d /run/media/drFaustroll/NICENANO/ ]; do
     sleep 1
     echo -n "."
