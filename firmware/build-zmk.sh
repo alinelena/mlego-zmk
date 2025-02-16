6f7935de7d91302a23984ad5dd5a5f1a1b449e3d
Alin Marin Elena <alin@elena.re>
2025-02-16 10:05:33 +0000

    fix pedantic stuff, extend script to build locally too with external modules

--- firmware/build-zmk.sh
+++ firmware/build-zmk.sh
@@ -4,12 +4,14 @@
 l=$PWD
 zmk_folder="$HOME/lavello/zmk/app"
 zmk_config="$HOME/playground/keyboard/mlego-zmk/config/"
+
+zmk_extra="$HOME/lavello/zmk-helpers"
 pushd $zmk_folder
 
 
-build_folder="xiao_rp2040_rev7"
-board="seeeduino_xiao_rp2040"
-shield="mlego5x13_rev7"
+#build_folder="xiao_rp2040_rev7"
+#board="seeeduino_xiao_rp2040"
+#shield="mlego5x13_rev7"
 
 
 #build_folder="xiao_ble_61_rev8"
@@ -27,20 +29,24 @@ shield="mlego5x13_rev7"
 for s in "ls013b7dh05" ; do
 #for s in "ls013b7dh03s" ; do
 #for s in "ls011b7dh03" ; do
-shield="mlego_m66_rev4 $s"
-board="nice_nano_v2"
-build_folder="mlego_m66_rev4${shield/ /_}"
+  shield="mlego_m66_rev4 $s"
+  board="nice_nano_v2"
+  build_folder="mlego_m66_rev4${shield/ /_}"
 
 
-rm -rf "$build_folder"
+  rm -rf "$build_folder"
 
-west build -d "$build_folder" -p always -b $board -- -DSHIELD="$shield" -DZMK_CONFIG=$zmk_config
-[[ -f "$build_folder/zephyr/zmk.uf2" ]] && cp "$build_folder/zephyr/zmk.uf2" "$l/$build_folder.uf2"
+  west build -d "$build_folder" -p always -b $board -S studio-rpc-usb-uart -- -DSHIELD="$shield" -DZMK_CONFIG=$zmk_config -DZMK_EXTRA_MODULES=$zmk_extra -DCONFIG_ZMK_STUDIO=y
+  [[ -f "$build_folder/zephyr/zmk.uf2" ]] && cp "$build_folder/zephyr/zmk.uf2" "$l/$build_folder.uf2"
 done
+
 while [ ! -d /run/media/drFaustroll/NICENANO/ ]; do
-    sleep 1
-    echo -n "."
+  sleep 1
+  echo -n "."
 done
+
 echo "done"
+
 set -x
+
 cp "$l/$build_folder.uf2" /run/media/drFaustroll/NICENANO/
